af97caa491397832ed93d95c24db502a8302711f
Flavio De Souza Git aula <flaviosf10@hotmail.com>
2025-01-31 09:36:29 -0300

    Terceiro commit

---
+++ exemplo_00.py
@@ -0,0 +1,2 @@
+#Crie um prograna que o usuário digite seu nome e retorne o número de caracteres
+print(len(input ("Digite seu nome: ")))
---
+++ exemplo_01.py
@@ -0,0 +1,4 @@
+valor_01 = int(input("Digite o primeiro número valor: "))
+valor_02 = int(input("Digite o segundo numero valor: "))
+total = valor_01 + valor_02
+print(total)
